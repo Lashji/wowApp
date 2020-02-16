09fafa27651e6e91247fc7fb71bb5463c59dc48d
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2020-02-16 20:14:09 +0200

    changed to use own backend instead

--- src/api/AppProvider.jsx
+++ src/api/AppProvider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getToken, getPVPLeaderBoard, doCharRequest } from "./ApiHandler";
+import { getData } from "./ApiHandler";
 
 export const AppContext = React.createContext();
 
@@ -10,29 +10,28 @@ export class AppProvider extends React.Component {
   }
 
   getAuthToken = async () => {
-    return await getToken();
+    return await getData("3v3");
   };
 
   async buildLeaderboard(data) {
-    let entries = data.data.entries.slice(0, 10);
+    let entries = data;
     console.log(entries);
 
-    let promises = [];
+    // let promises = [];
 
-    for (let i = 0; i < entries.length; i++) {
-      let region = "eu";
-      let realm = entries[i].character.realm.slug;
-      let name = entries[i].character.name;
-      if (!realm || !name) continue;
-      let char = await doCharRequest(region, realm, name, this.state.token);
-      promises.push(char);
-    }
+    // for (let i = 0; i < entries.length; i++) {
+    //   let region = "eu";
+    //   let realm = entries[i].character.realm.slug;
+    //   let name = entries[i].character.name;
+    //   if (!realm || !name) continue;
+    //   promises.push(char);
+    // }
 
-    let d = await Promise.all(promises);
-    console.log("promis data", d);
-    this.setState({
-      leaderboard3: d
-    });
+    // let d = await Promise.all(promises);
+    // console.log("promis data", d);
+    // this.setState({
+    //   leaderboard3: d
+    // });
   }
 
   componentDidMount = () => {
@@ -41,10 +40,10 @@ export class AppProvider extends React.Component {
         this.setState({ token: results.data });
 
         if (!this.state.token) return;
-        let leaderboard = getPVPLeaderBoard(this.state.token).then(res => {
-          console.log("leaderboard: ", res);
-          this.buildLeaderboard(res);
-        });
+        // let leaderboard = getPVPLeaderBoard(this.state.token).then(res => {
+        //   console.log("leaderboard: ", res);
+        //   this.buildLeaderboard(res);
+        // });
         //   console.log("auth token ", results);
       });
   };
